0130b7c13758cfdcebeef92723f9ee2a2e734cb6
Hitoshi Harumi <hrmhts0404@gmail.com>
2021-09-30 16:09:23 +0900

    Improve Readability, adding comments, re-naming.

--- platonicsvg.js
+++ platonicsvg.js
@@ -1,60 +1,85 @@
 /*
-MIT License
-Copyright (c) 2021 Hitoshi Harumi
+  MIT License
+  Copyright (c) 2021 Hitoshi Harumi
 
-This software is released under the MIT License, see ./LICENSE
+  This software is released under the MIT License, see ./LICENSE
 */
 class PlatonicSVG extends HTMLElement {
+
 
   constructor() {
     super();
 
-    this.degree = Math.PI/180;
-    this.rotatedDegree = [0,0,0];
+    this.degree   = Math.PI/180;// Constant to use frequently.
+    this.timerId  = null;       // To remove rendering callback loop when disconned.
 
-    this.faces    = null;
-    this.svgFaces = [];
-    this.timerId  = null;
+    this.rotatedDegree = [0,0,0];// How much rotated from the "base faces."
+    this.baseFaces     = null;   // Base coordinates of a solid's faces.
+    this.svgFaces      = [];     // Actual SVG objects rendered on a screen whos
+                                 // calculated by "rotatedDegree" and "baseFaces".
+    
+    this.firstChildSVG = null;   // For fast access to <this.firstChild>
   }
 
+
   connectedCallback() {
-    this.faces = this.setVertices(this.getAttribute("solid") || "12");
-    this.progressMax = parseInt(this.getAttribute("progress-max") || 100); 
-    
-    const width  = this.getAttribute("width")  || "256";
-    const height = this.getAttribute("height") || "256";
-    const backc  = this.getAttribute("back-color") || "inherit";
-    const linec  = this.getAttribute("edge-color") || "#6E777C";
-    const linew  = this.getAttribute("edge-width") || "1px";
-    const facec  = this.getAttribute("face-color") || "rgba(215,230,244, 0.8)";
+    // Params for initializing from attributes or default.
 
-    const containerCls = this.getAttribute("SVGClassName")  || "platonic-svg";
-    const facesCls     = this.getAttribute("PolyhedronFaceClassName") || "platonic-face";
+    // Define view which of Platon Solids, controlling with "solid" attr.
+    // e.g) 12, dodeca, dodecahedron  all show dodecahedron.
+    this.baseFaces = this.getInitialCoordinates(
+      this.getAttribute("solid") || "12" 
+    ); 
 
-    this.setSVG(width, height, backc, linec, facec, linew,containerCls, facesCls);
-
+    // Define a direction the solid spin in 3D, and fps of a "tick".
     const dx  = parseInt((this.getAttribute("dx")  || 1));
     const dy  = parseInt((this.getAttribute("dy")  || 1));
     const dz  = parseInt((this.getAttribute("dz")  || 0));
     const fps = parseInt((this.getAttribute("fps") || 40));
-    this.timerId = setInterval(() => {
-      this.rotate(dx, dy, dz);
-      this.render();
-    }, (1000/fps));
-  }
+    
+    // For SVG solid representings, such as width, color, edge-width...etc.
+    const width  = this.getAttribute("width")      || "256";
+    const height = this.getAttribute("height")     || "256";
+    const backc  = this.getAttribute("back-color") || "inherit";
+    const edgec  = this.getAttribute("edge-color") || "#6E777C";
+    const edgew  = this.getAttribute("edge-width") || "1px";
+    const facec  = this.getAttribute("face-color") || "rgba(215,230,244, 0.8)";
+    // For SVG elements class name for let CSS can control them.
+    const containerCls = this.getAttribute("SVGClassName")  || "platonic-svg";
+    const facesCls     = this.getAttribute("PolyhedronFaceClassName") || "platonic-face";
 
+
+    // Create SVG Element and set them to this.svgFaces .
+    // Almost of all setups get done.
+    this.constructSVGElements(
+      width, height,
+      backc, edgec, facec, edgew,
+      containerCls, facesCls
+    );
+
+    // For progress-bar like facility.
+    // TODO: Is this facility necessary??
+    this.progressMax = parseInt(this.getAttribute("progress-max") || 100);
+
+
+    // Start to Rotate!
+    this.timerId = setInterval(
+      () => { this.rotate(dx, dy, dz); this.render();},
+      1000/fps
+      );
+  }
   disconnectedCallback() {
     clearInterval(this.timerId);
   }
 
-  static get observedAttributes() {
-    return ["progress-value"]
-  }
 
-  attributeChangedCallback(_n, _o, nv) {
-    const progressValue = parseInt(nv);
+  // For progress-bar like facility.
+  // TODO: Is needed?
+  static get observedAttributes() {return ["progress-value"];}
+  attributeChangedCallback(_n, _o, newvalue) {
+    
     // get the integer part of calculated progressed counts.
-    const latest = ((nv / this.progressMax) * this.svgFaces.length) | 0;
+    const latest = ((newvalue / this.progressMax) * this.svgFaces.length) | 0;
     const progressedFill = this.getAttribute("progressed-face-color") || "rgba(0,0,0,0.6)";
     const defaultFill = this.getAttribute("face-color") || "rgba(215,230,244, 0.8)";
 
@@ -76,78 +101,94 @@ class PlatonicSVG extends HTMLElement {
     }
   }
 
-  setVertices(solidType) {
+  getInitialCoordinates(solidType) {
 
-    let vertices;
+    let coordinates;
 
-    switch(solidType){
+    switch(solidType) {
+
       case '4':
       case 'tetra':
       case 'tetrahedron':
       case 'simplex':
-        vertices = this.unitTetrahedron();
+        coordinates = this.unitTetrahedron();
         break;
 
       case '6':
       case 'hexa':
       case 'hexahedron':
       case 'cube':
-        vertices = this.unitHexahedron();
+        coordinates = this.unitHexahedron();
         break;
 
       case '8':
       case 'octa':
       case 'octahedron':
-        vertices = this.unitOctahedron();
+        coordinates = this.unitOctahedron();
         break;
 
       case '12':
       case 'dodeca':
       case 'dodecahedron':
-        vertices = this.unitDodecahedron();
+        coordinates = this.unitDodecahedron();
         break;
 
       case '20':
       case 'icosa':
       case 'icosahedron':
-        vertices = this.unitIcosahedron();
+        coordinates = this.unitIcosahedron();
         break;
     }
 
-    return vertices.map(
+    // Return a 128 times enlarged coordinates of the solid.
+    // Number 128 has no meanings, but  
+    return coordinates.map(
       (face)=>{
-        return face.map(([x,y,z])=>
-          [128*x, 128*y, 128*z]
-        );
+        return face.map( ([x,y,z]) => [128*x, 128*y, 128*z]);
       }
     );
   }
-  setSVG(width, height, backc, linec, facec, linew, containerCls, facesCls) {
-    const svgEl = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    
-    svgEl.setAttribute('width',  width);
-    svgEl.setAttribute('height', height);
-    svgEl.setAttribute('viewBox', '-128 -128 256 256');
-    svgEl.setAttribute('style', ("background-color:"+backc+';') );
 
-    svgEl.classList.add(containerCls);
+  constructSVGElements(width, height, backc, edgec, facec, edgew,
+                       containerCls, facesCls) {
+    const containerSVG =
+      document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  
+    // Set view configs on the container elems.   
+    containerSVG.setAttribute('width'  , width);
+    containerSVG.setAttribute('height' , height);
+    containerSVG.setAttribute('viewBox', '-128 -128 256 256');
+    containerSVG.setAttribute('style'  , ("background-color:"+backc+';'));
+    containerSVG.classList.add(containerCls);
 
-    this.faces.forEach((face)=>{
-      const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-      polygon.classList.add(facesCls);
-      polygon.setAttribute("stroke-width",linew)
-      polygon.setAttribute("stroke",linec);
+    // Create SVG polygons, which represent current positioned faces of solid.
+    // And Append them into svgFaces, and containerSVG.
+    this.baseFaces.forEach((face)=>{
+
+      // Current Face of a solid.
+      const polygon = 
+        document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+      
+      // Set view configs on a polygon ()
+      polygon.setAttribute("stroke-width",edgew)
+      polygon.setAttribute("stroke",edgec);
       polygon.setAttribute("fill",facec);
+      polygon.classList.add(facesCls);
+      // Put the "coordinates info" into.
       polygon.setAttribute(
           "points",
           face.map(this.zProject).toString()
         );
+
+      // svgFaces     is ref to polygons for later operations.
+      // containerSVG is actual seen SVG on a screen.
       this.svgFaces.push(polygon);
-      svgEl.appendChild(polygon);
+      containerSVG.appendChild(polygon);
     });
 
-    this.appendChild(svgEl);
-    this.svgEl = this.firstChild;
+    // Append the container and get the reference.
+    this.appendChild(containerSVG);
+    this.firstChildSVG = this.firstChild;
   }
 
   rotate(dx, dy, dz) {
@@ -157,11 +198,26 @@ class PlatonicSVG extends HTMLElement {
   }
 
   render() {
-    
-    this.clearChild(this.svgEl);
+    // Clear current SVG polygons, 
+    // Create new SVG polygons with updated coordinates,
+    // And append them to the container
 
+    /*
+    **  render() calculates next moment's coordinates by two info:
+    **    this.baseFaces     : the initial coordinates of a solid
+    **    this.rotatedDegree : how much rotated from the initial.
+    **
+    **  instead of using (rotation vector) and (current coordinates)
+    **  to avoid to solid gently shrink (caused by cancellation stuff may be).
+    */
+
+
+    // Clear the currents.
+    this.clearChild(this.firstChildSVG);
+
+
+    // Create New 
     const [x, y, z] = this.rotatedDegree;
-
     const deg  = this.degree;
     const cosX = Math.cos(deg * x);
     const sinX = Math.sin(deg * x);
@@ -171,49 +227,57 @@ class PlatonicSVG extends HTMLElement {
     const sinZ = Math.sin(deg * z);
 
     let i = 0;
-    this.faces.forEach((face)=>{
-      const vertices = face.map((vertex)=>[
+    this.baseFaces.forEach((face)=>{
+      
+      const coordinates = face.map((vertex)=>[
         (cosY*cosZ)*vertex[0]  +(sinX*sinY*cosZ - cosX*sinZ)*vertex[1]  +(cosX*sinY*cosZ + sinX*sinZ)*vertex[2],
         (cosY*sinZ)*vertex[0]  +(sinX*sinY*sinZ + cosX*cosZ)*vertex[1]  +(cosX*sinY*sinZ - sinX*cosZ)*vertex[2],
             (-sinY)*vertex[0]                   +(sinX*cosY)*vertex[1]                   +(cosX*cosY)*vertex[2]
         ]);
-      this.svgFaces[i].setAttribute(
-          "points",
-          vertices.map(this.zProject).toString()
-        );
-      const flag = (i == 10);
-      this.svgFaces[i].z = this.getZAxis(vertices, flag);
+      
+      this.svgFaces[i].setAttribute( "points", coordinates.map(this.zProject).toString() );
+      // Add z property for sorting them later.
+      this.svgFaces[i].z = this.getZIndex(coordinates);
+      
       i++;
     });
 
+
+    // Little Hacky.
+    // Before appending new polygons, sort them by z-prop calculated in for loop.
+    // Becaus SVG does NOT support Z index property now, 
+    // order of child elements is the only way to represent Z index.
     const polygons = this.svgFaces.slice();
     polygons.sort(this.sortByZ);
-
     for(const p of polygons) {
-      this.svgEl.appendChild(p);
+      this.firstChildSVG.appendChild(p);
     }
 
   }
 
-  zProject([x,y,z]){return [x,y]}
 
-  //helpers
-  getZAxis(vertices, flag) {
+  getZIndex(faceCoordinates) {
     let sum = 0;
-    vertices.forEach((vertex)=>{
-      sum += vertex[2];
+    faceCoordinates.forEach((vertex)=>{
+      sum += vertex[2]; // sum z of each points of a face. 
     });
-    sum *= 1000000;// is this adequate?
+    sum *= 8096;// TODO: is this adequate?
     return sum | 0;// retrieve integer part
   }
-  sortByZ(a,b){
+  sortByZ(a,b){ 
     return(a.z - b.z);
   }
+
+  //helpers
+  zProject([x,y,z]){return [x,y]}
   clearChild(node) {
     while(node.firstChild){
       node.firstChild.remove()
     }
   }
+
+  // For initial coordinates of Platon Solids.
+  // Very beutiful.
   unitTetrahedron() {
     const r2 = Math.sqrt(2);
     const r6 = Math.sqrt(6);
@@ -228,7 +292,7 @@ class PlatonicSVG extends HTMLElement {
       [a,b,d],
       [a,c,d],
       [b,c,d]
-    ]
+    ];
   }
   unitHexahedron() {
     const l = (1/3)*Math.sqrt(3);
@@ -266,13 +330,13 @@ class PlatonicSVG extends HTMLElement {
   unitDodecahedron() {
     const ratio = Math.sqrt(0.5 + Math.sqrt(5)/6) / (2 + Math.sqrt(5));
     const alpha = ((1 + Math.sqrt(5))/2) * ratio;
-    const beta =   (2 + Math.sqrt(5))    * ratio;
+    const beta  =  (2 + Math.sqrt(5))    * ratio;
     const gamma = ((3 + Math.sqrt(5))/2) * ratio;
 
-    const t1 = [ alpha, 0, beta];
-    const t2 = [-alpha, 0, beta];
-    const b1 = [ alpha, 0,-beta];
-    const b2 = [-alpha, 0,-beta];
+    const t1  = [ alpha, 0, beta];
+    const t2  = [-alpha, 0, beta];
+    const b1  = [ alpha, 0,-beta];
+    const b2  = [-alpha, 0,-beta];
 
     const mt1 = [ gamma, gamma, gamma];
     const mt2 = [     0,  beta, alpha];
@@ -311,9 +375,8 @@ class PlatonicSVG extends HTMLElement {
   }
   unitIcosahedron() {
     const ratio = Math.sqrt( 2/(5+Math.sqrt(5) ));
-    const unit = 1 * ratio;
-    const gr = ( (1+Math.sqrt(5))/2 ) * ratio;
-
+    const unit  = 1 * ratio;
+    const gr    = ( (1+Math.sqrt(5))/2 ) * ratio;
 
     const t1 = [    0, unit,   gr];
     const t2 = [  -gr,    0, unit];
